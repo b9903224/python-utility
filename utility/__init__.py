e24ed7cc2ee6c2f0820cbc2d6d0220bf2ee605f0
b9903224 <b9903224@gmail.com>
2019-11-02 21:13:28 +0800

    Add files via upload

--- utility/__init__.py
+++ utility/__init__.py
@@ -6,8 +6,10 @@ Created on Mon Sep 30 18:09:53 2019
 """
 
 from .printUniqueRatio import printUniqueRatio
+from .getMergedDemoImg import getMergedDemoImg
 
-__all__ = ['printUniqueRatio']
+__all__ = ['printUniqueRatio',
+           'getMergedDemoImg']
 
 #__all__ = ['printUniqueRatio',
 #           'draw_haar_like_feature']
