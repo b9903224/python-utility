a43a313d9dd64c5a478754d1670b74ab0749c596
b9903224 <b9903224@gmail.com>
2019-11-02 21:12:00 +0800

    Add files via upload

--- utility/getMergedDemoImg.py
+++ utility/getMergedDemoImg.py
@@ -6,7 +6,20 @@ Created on Sat Nov  2 15:39:15 2019
 """
 
 import numpy as np
+from PIL import ImageFont, ImageDraw, Image
 
+'''
+font_background_rgb: unused
+'''
+#import time
+#start = time.time()
+#time.sleep(5.5)
+#end = time.time()
+#print(end - start)
+
+
+
+##
 def getMergedDemoImg(row_num, col_num, imgH, imgW,
                      img_list, title_list, gap_ver, gap_hoz,
                      font_path, font_size, font_rgb, font_background_rgb,
@@ -16,12 +29,68 @@ def getMergedDemoImg(row_num, col_num, imgH, imgW,
     mergedDemoImg = iniMergedDemoImg(row_num * (imgH + gap_ver) + append_height,
                                      col_num * (imgW + gap_hoz), demo_background_rgb)
     mergedDemoImg = insert_img_list(mergedDemoImg, img_list, imgH, imgW, gap_ver, gap_hoz)
-#    mergedDemoImg = insert_title_list(mergedDemoImg)
-            
     
+    
+    # insert text
+    font = get_font(font_path, font_size)
+    mergedDemoImg = insert_title_list(mergedDemoImg, title_list, imgH, imgW, gap_ver, gap_hoz,
+                                      font_path, font_size, font_rgb, font)
+    mergedDemoImg = insert_descp_text(mergedDemoImg, descp_text, imgH, imgW,
+                                      descp_test_start_y, descp_test_start_x,
+                                      font_size, font_rgb, font)
     
     return mergedDemoImg
 
+##
+def insert_descp_text(mergedDemoImg, descp_text, imgH, imgW,
+                      descp_test_start_y, descp_test_start_x, 
+                      font_size, font_rgb, font):
+    
+    frame = Image.fromarray(mergedDemoImg)
+    draw = ImageDraw.Draw(frame)
+    
+    for row_num, descp_row in enumerate(descp_text):
+        y0 = descp_test_start_y[row_num]
+        x = descp_test_start_x[row_num]
+        for col_num, descp in enumerate(descp_row):
+            y = y0 + font_size*col_num
+            print(y)
+            if len(descp) == 0:
+                continue
+            draw.text((x,y), descp, tuple(font_rgb), font=font)
+    
+    mergedDemoImg = np.array(frame)
+    return mergedDemoImg
+
+##
+def insert_title_list(mergedDemoImg, title_list, imgH, imgW, gap_ver, gap_hoz,
+                      font_path, font_size, font_rgb, font):
+    
+    frame = Image.fromarray(mergedDemoImg)
+    draw = ImageDraw.Draw(frame)
+    
+    for row_num, title_row in enumerate(title_list):
+        y = row_num * (imgH + gap_ver)
+        for col_num, title in enumerate(title_row):
+            x = col_num * (imgW + gap_hoz)
+            if len(title) == 0:
+                continue
+            draw.text((x,y), title, tuple(font_rgb), font=font)
+    
+    mergedDemoImg = np.array(frame)
+    return mergedDemoImg
+
+##
+def get_font(font_path, font_size):
+    
+    if len(font_path) == 0:
+        font = ImageFont.load_default()
+    else:
+        font = ImageFont.truetype(font_path, font_size)
+        
+    return font
+
+##
 def insert_img_list(mergedDemoImg, img_list, imgH, imgW, gap_ver, gap_hoz):
     
     for row_num, img_row in enumerate(img_list):
@@ -49,8 +118,7 @@ def insert_img_list(mergedDemoImg, img_list, imgH, imgW, gap_ver, gap_hoz):
                     
     return mergedDemoImg
             
-            
-
+##
 def iniMergedDemoImg(imgH, imgW, demo_background_rgb):
     
     mergedDemoImg = np.zeros((imgH, imgW, 3), dtype=np.uint8)
