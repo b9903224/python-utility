43c736019d94f4e25548d5b9613a0908101967a4
b9903224 <b9903224@gmail.com>
2020-01-05 23:28:54 +0800

    fix imshow: add plt(..., vmin, vmax) by dtype add imshow_demo add gitignore for __pycache__

--- utility/imshow.py
+++ utility/imshow.py
@@ -6,14 +6,26 @@ Created on Sun Jan  5 15:57:08 2020
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 def imshow(img, imgName='img', isFullScreen=False):
     plt.figure()
-    plt.imshow(img, cmap=plt.cm.gray)
-    plt.title('%s: (%gX%gX%g)\ndtype:%s, max:%g, min:%g'%(imgName, *getImgSize(img), img.dtype, img.max(), img.min()))
+    img.dtype
+    vmin = 0
+    vmax = 1
+    if img.dtype == np.bool:
+        vmin = 0
+        vmax = 1
+    elif img.dtype == np.uint8:
+        vmin = 0
+        vmax = 255
+        
+    plt.imshow(img, cmap=plt.cm.gray, vmin=vmin, vmax=vmax)
+    plt.title('%s: (%gX%gX%g), dtype:%s\nimg range: [%g, %g], imshow range: [%g, %g]'%(imgName, *getImgSize(img), img.dtype, img.max(), img.min(), vmin, vmax))
     if isFullScreen:
         figManager = plt.get_current_fig_manager()
         figManager.window.showMaximized()
+    return plt
 def getImgSize(img):
     imgZ = 0
     imgH = img.shape[0]
