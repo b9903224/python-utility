7c2edc0b2c9864d6a72f590629682a06dc710097
b9903224 <b9903224@gmail.com>
2020-01-08 01:30:14 +0800

    getHighLightImgByRange

--- getHighLightImgByRange_demo.py
+++ getHighLightImgByRange_demo.py
@@ -13,10 +13,16 @@ stringImg = utility.getStringImg_20(text)
 
 img_rgb = io.imread('.\demo\getHighLightImgByRange\duck.jpg')
 img_gray = (rgb2gray(img_rgb) * 255).astype(np.uint8)
-img_bw = img_gray < 250
+img_bw = img_gray > 250
 
-yStart = 200
-xStart = 70
+yStart = 100
+xStart = 80
+#yStart = 340
+#xStart = 330
+#yStart = 335
+#xStart = 4
+#yStart = 4
+#xStart = 335
 img_rgb_demo = utility.getHighLightImgByRange(stringImg, img_rgb, yStart, xStart)
 img_rgb_demo2 = utility.getHighLightImgByRange(stringImg, img_rgb, yStart, xStart, rgb=[0, 0, 50], isTransparent=True, rgbBackground=[50,50,0])
 img_gray_demo = utility.getHighLightImgByRange(stringImg, img_gray, yStart, xStart)
@@ -24,10 +30,12 @@ img_gray_demo2 = utility.getHighLightImgByRange(stringImg, img_gray, yStart, xSt
 img_bw_demo = utility.getHighLightImgByRange(stringImg, img_bw, yStart, xStart)
 img_bw_demo2 = utility.getHighLightImgByRange(stringImg, img_bw, yStart, xStart, rgb=[0, 0, 50], isTransparent=True, rgbBackground=[50,50,0])
 
-utility.imshow(stringImg)
-utility.imshow(img_rgb)
-utility.imshow(img_gray)
-utility.imshow(img_bw)
+path = r'.\demo\getHighLightImgByRange'
+dpi = 512
+utility.imshow(stringImg).savefig(r'%s\%s.png'%(path, 'stringImg'), dip=dpi)
+utility.imshow(img_rgb).savefig(r'%s\%s.png'%(path, 'img_rgb'), dip=dpi)
+utility.imshow(img_gray).savefig(r'%s\%s.png'%(path, 'img_gray'), dip=dpi)
+utility.imshow(img_bw).savefig(r'%s\%s.png'%(path, 'img_bw'), dip=dpi)
 
 utility.imshow(img_rgb_demo)
 utility.imshow(img_rgb_demo2)
@@ -36,6 +44,10 @@ utility.imshow(img_gray_demo2)
 utility.imshow(img_bw_demo)
 utility.imshow(img_bw_demo2)
 
-
-
+io.imsave(r'%s\%s.png'%(path, 'img_rgb_demo'), img_rgb_demo)
+io.imsave(r'%s\%s.png'%(path, 'img_rgb_demo2'), img_rgb_demo2)
+io.imsave(r'%s\%s.png'%(path, 'img_gray_demo'), img_gray_demo)
+io.imsave(r'%s\%s.png'%(path, 'img_gray_demo2'), img_gray_demo2)
+io.imsave(r'%s\%s.png'%(path, 'img_bw_demo'), img_bw_demo)
+io.imsave(r'%s\%s.png'%(path, 'img_bw_demo2'), img_bw_demo2)
 
--- utility/getHighLightImgByRange.py
+++ utility/getHighLightImgByRange.py
@@ -8,5 +8,53 @@ Created on Wed Jan  8 00:16:40 2020
 import numpy as np
 
 def getHighLightImgByRange(destImg, sourceImg, yStart, xStart, rgb=[255, 0, 0], isTransparent=False, rgbBackground=[]):
-    resultImg = np.copy(sourceImg)
+
+    
+    resultImg = iniResultImg(sourceImg)
+#    destImg_inv = np.invert(destImg)
+    
+    imgH_src, imgW_src, imgZ_src = getImgSize(sourceImg)
+    imgH_dest, imgW_dest, imgZ_dest = getImgSize(destImg)
+    yEnd = min(yStart+imgH_dest-1, imgH_src-1)
+    xEnd = min(xStart+imgW_dest-1, imgW_src-1)
+    destImg_sub = destImg[0:yEnd-yStart+1, 0:xEnd-xStart+1]
+    resultImg_sub = resultImg[yStart:yEnd+1,xStart:xEnd+1,:]
+    destImg_sub_inv = np.invert(destImg_sub)
+    for ch in range(3):
+        resultImg_sub_ch = resultImg_sub[:,:,ch]
+        if not isTransparent:
+            resultImg_sub_ch[destImg_sub] = rgb[ch]
+        else:
+            resultImg_sub_ch[destImg_sub] = resultImg_sub_ch[destImg_sub] + rgb[ch]
+#            resultImg_ch[destImg] = resultImg_ch[destImg] + 255
+        if rgbBackground:
+            resultImg_sub_ch[destImg_sub_inv] = resultImg_sub_ch[destImg_sub_inv] + rgbBackground[ch]
+        resultImg_sub[:,:,ch] = resultImg_sub_ch
+    
+    resultImg[yStart:yEnd+1,xStart:xEnd+1,:] = resultImg_sub
+    resultImg = finalizeResultImg(resultImg)
     return resultImg
+
+def finalizeResultImg(resultImg):
+    resultImg = np.minimum(resultImg, 255)
+    resultImg = np.maximum(resultImg, 0)
+    resultImg = resultImg.astype(np.uint8)    
+    return resultImg
+
+def iniResultImg(sourceImg):
+    resultImg = np.copy(sourceImg)
+    if resultImg.dtype == np.bool:
+        resultImg = resultImg.astype(np.uint8)*255
+    imgH, imgW, imgZ = getImgSize(resultImg)
+    if imgZ == 0:
+        resultImg = np.dstack((resultImg, resultImg, resultImg))
+    resultImg = resultImg.astype(np.float)
+    return resultImg
+
+def getImgSize(img):
+    imgZ = 0
+    imgH = img.shape[0]
+    imgW = img.shape[1]
+    if img.ndim == 3:
+        imgZ = img.shape[2]
+    return imgH, imgW, imgZ
